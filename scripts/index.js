15e7b0abd72ee4120dd1f1a81ee67989256e8578
lironghe <lironghe@infobeats.cn>
2019-09-10 15:09:05 +0800

    test blogs

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@
     //   marked('# Marked in browser\n\nRendered by **marked**.');
 
 ;(async () => {
-    let res = await $.get('/docs/docs/test1.md')
+    let res = await $.get('/blogs/docs/test1.md')
     console.log('res is:', res)
 
-    let json = await $.get('/docs/scripts/docs.json')
+    let json = await $.get('/blogs/scripts/docs.json')
     console.log('json is:', json)
 })()
  
